0a5e1aa1f14cc8384177761f9db5f93f98e0c2a1
slimaasj <salimjbara@gmail.com>
2022-11-06 18:49:36 -0500

    Add files via upload

--- py_imu/pyIMUpub.py
+++ py_imu/pyIMUpub.py
@@ -9,54 +9,6 @@ from sensor_msgs.msg import Imu
 from sensor_msgs.msg import MagneticField
 from transforms3d.euler import euler2quat as quaternion_from_euler
 
-RAD_TO_DEG = 57.29578
-M_PI = 3.14159265358979323846
-G_GAIN = 0.070          # [deg/s/LSB]  If you change the dps for gyro, you need to update this value accordingly
-AA =  0.40              # Complementary filter constant
-MAG_LPF_FACTOR = 0.4    # Low pass filter constant magnetometer
-ACC_LPF_FACTOR = 0.4    # Low pass filter constant for accelerometer
-ACC_MEDIANTABLESIZE = 9         # Median filter table size for accelerometer. Higher = smoother but a longer delay
-MAG_MEDIANTABLESIZE = 9         # Median filter table size for magnetometer. Higher = smoother but a longer delay
-
-gyroXangle = 0.0
-gyroYangle = 0.0
-gyroZangle = 0.0
-CFangleX = 0.0
-CFangleY = 0.0
-CFangleXFiltered = 0.0
-CFangleYFiltered = 0.0
-kalmanX = 0.0
-kalmanY = 0.0
-oldXMagRawValue = 0
-oldYMagRawValue = 0
-oldZMagRawValue = 0
-oldXAccRawValue = 0
-oldYAccRawValue = 0
-oldZAccRawValue = 0
-
-#Setup the tables for the mdeian filter. Fill them all with '1' so we dont get devide by zero error
-acc_medianTable1X = [1] * ACC_MEDIANTABLESIZE
-acc_medianTable1Y = [1] * ACC_MEDIANTABLESIZE
-acc_medianTable1Z = [1] * ACC_MEDIANTABLESIZE
-acc_medianTable2X = [1] * ACC_MEDIANTABLESIZE
-acc_medianTable2Y = [1] * ACC_MEDIANTABLESIZE
-acc_medianTable2Z = [1] * ACC_MEDIANTABLESIZE
-mag_medianTable1X = [1] * MAG_MEDIANTABLESIZE
-mag_medianTable1Y = [1] * MAG_MEDIANTABLESIZE
-mag_medianTable1Z = [1] * MAG_MEDIANTABLESIZE
-mag_medianTable2X = [1] * MAG_MEDIANTABLESIZE
-mag_medianTable2Y = [1] * MAG_MEDIANTABLESIZE
-mag_medianTable2Z = [1] * MAG_MEDIANTABLESIZE
-
-magXmin =  0
-magYmin =  0
-magZmin =  0
-magXmax =  0
-magYmax =  0
-magZmax =  0
-
-imu_msg = Imu()
-
 class PyIMUPub(Node):
     def __init__(self):
         super().__init__("pyIMUpub")
@@ -66,6 +18,54 @@ class PyIMUPub(Node):
             topic='imu',
             qos_profile=1,
         )
+        
+        RAD_TO_DEG = 57.29578
+        M_PI = 3.14159265358979323846
+        G_GAIN = 0.070          # [deg/s/LSB]  If you change the dps for gyro, you need to update this value accordingly
+        AA =  0.40              # Complementary filter constant
+        MAG_LPF_FACTOR = 0.4    # Low pass filter constant magnetometer
+        ACC_LPF_FACTOR = 0.4    # Low pass filter constant for accelerometer
+        ACC_MEDIANTABLESIZE = 9         # Median filter table size for accelerometer. Higher = smoother but a longer delay
+        MAG_MEDIANTABLESIZE = 9         # Median filter table size for magnetometer. Higher = smoother but a longer delay
+
+        gyroXangle = 0.0
+        gyroYangle = 0.0
+        gyroZangle = 0.0
+        CFangleX = 0.0
+        CFangleY = 0.0
+        CFangleXFiltered = 0.0
+        CFangleYFiltered = 0.0
+        kalmanX = 0.0
+        kalmanY = 0.0
+        oldXMagRawValue = 0
+        oldYMagRawValue = 0
+        oldZMagRawValue = 0
+        oldXAccRawValue = 0
+        oldYAccRawValue = 0
+        oldZAccRawValue = 0
+
+        #Setup the tables for the mdeian filter. Fill them all with '1' so we dont get devide by zero error
+        acc_medianTable1X = [1] * ACC_MEDIANTABLESIZE
+        acc_medianTable1Y = [1] * ACC_MEDIANTABLESIZE
+        acc_medianTable1Z = [1] * ACC_MEDIANTABLESIZE
+        acc_medianTable2X = [1] * ACC_MEDIANTABLESIZE
+        acc_medianTable2Y = [1] * ACC_MEDIANTABLESIZE
+        acc_medianTable2Z = [1] * ACC_MEDIANTABLESIZE
+        mag_medianTable1X = [1] * MAG_MEDIANTABLESIZE
+        mag_medianTable1Y = [1] * MAG_MEDIANTABLESIZE
+        mag_medianTable1Z = [1] * MAG_MEDIANTABLESIZE
+        mag_medianTable2X = [1] * MAG_MEDIANTABLESIZE
+        mag_medianTable2Y = [1] * MAG_MEDIANTABLESIZE
+        mag_medianTable2Z = [1] * MAG_MEDIANTABLESIZE
+
+        magXmin =  0
+        magYmin =  0
+        magZmin =  0
+        magXmax =  0
+        magYmax =  0
+        magZmax =  0
+
+        imu_msg = Imu()
 
         # TODO arrays not supported as parameter type ROS2
         imu_msg.orientation_covariance = [0.0025, 0.0, 0.0,
